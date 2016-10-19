751d3d1c6c21a751edce0269d67c308684c58c48
david <david.l.castilla@gmail.com>
2016-10-19 11:38:10 -0400

    add function that deals with sorting data by date

--- dataVisHelpers.js
+++ dataVisHelpers.js
@@ -12,4 +12,21 @@ const generateDPath = (data, scale) => {
   return path
 }
 
+// passing array of objects with keys value and date
+const generateDPath = (data, scale) => {
+  data = data.sort((a,b) => {return a.date - b.date})
+
+  const maxY = Math.max.apply(Math, data.map(function(o){return o.value;}))
+  const minY = Math.min.apply(Math, data.map(function(o){return o.value;}))
+
+  const maxX = Math.max.apply(Math, data.map(function(o){return o.date;}))
+  const minX = Math.min.apply(Math, data.map(function(o){return o.date;}))
+    
+  let path = "";  
+  for (var i = 0; i < data.length; i++) {
+    path += " L" + ((data[i].date-minX)/(maxX-minX))*scale.width + " " + (scale.height - (data[i].value-minY)/(maxY-minY)*scale.height);
+  };
+  return path
+}
+
 export default generateDPath;
